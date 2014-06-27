9247e1c007a50d8c2529695ca94ac18e124d8d84
erdvillegas <vi.erik@yahoo.com>
2014-06-26 23:08:59 -0500

    Templates Funcional

--- config.py
+++ config.py
@@ -10,15 +10,6 @@ DEBUG_SITE = True
 #Rutas Path
 RUTA_PROYECTO = Path(__file__).ancestor(1)
 TEMPLATE_PATH = RUTA_PROYECTO.child('templates')
-STATIC_FILES = RUTA_PROYECTO.child('static')
+STATIC_FILES = '/static/'
 
 #Error Handler
-
-vat = {
-	'BASE_DIR' : BASE_DIR,
-	'DEBUG_SITE' : DEBUG_SITE,
-	'RUTA_PROYECTO' : RUTA_PROYECTO,
-	'TEMPLATE_PATH' : TEMPLATE_PATH,
-	'STATIC_FILES' : STATIC_FILES,
-	'TEST' : os.getcwd(),
-}
