519a569a721ce5c9f248026c5c70c050d2367033
erdvillegas <vi.erik@yahoo.com>
2014-06-30 01:19:21 -0500

    Primera parte HandlerError

--- handlers/ErrorHandler.py
+++ handlers/ErrorHandler.py
@@ -1,13 +1,37 @@
+import jinja2
 import logging
+import os
+import urllib
 import webapp2
-from webapp2_extras import jinja2
+
+
+from handlers import BaseHandler
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+def render_error(template_values):
+	 template = JINJA_ENVIRONMENT.get_template('error.html')
+	 response.write(template.render(template_values))
+
 
 def handle_404(request, response, exception):
     logging.exception(exception)
-    response.write('Oops! I could swear this page was here!')
     response.set_status(404)
+    template_values ={
+    'tituloError' : 'Algo ha pasado ',
+    'mensaje' : 'Oops! La pagina ya no esta por aqui'
+    }
+    render_error(template_values)
 
 def handle_500(request, response, exception):
     logging.exception(exception)
-    response.write('AUn error')
     response.set_status(500)
+    template_values ={
+    'tituloError' : 'Algo ha pasado ',
+    'mensaje' : ':( Algo paso con la pagina'
+    }
+    render_error(template_values)
