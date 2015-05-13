8f554a233df4f0cd4b37950515db3a65a6b69d58
erdvillegas <vi.erik@yahoo.com>
2015-05-13 00:14:01 -0500

    Depurando contenido innecesario

--- handlers/siteHandler.py
+++ handlers/siteHandler.py
@@ -7,6 +7,7 @@ import webapp2
 
 from config import *
 from lib import httpagentparser
+from handlers.BaseHandler import *
 from webapp2_extras import jinja2
 from webapp2_extras import json
 from webapp2 import Router
@@ -14,35 +15,30 @@ from lib.ua_parser import *
 from lib.user_agents import parse
 from google.appengine.ext.webapp import template
 
+ 
+class MainHandler(BaseHandler):
+    def get(self):
+        from webapp2 import Route
+        template_val ={
+        'metaDescripcion' : 'Consultorio general Gerardo Sepulveda',
+        'metaKeyWords' : 'consultorio,gerardo,sepulveda',
+        'metaCreador' : 'Erik Villegas'
+        }
+        self.render_template('index.html',**template_val)
+    def login(self):
+        self.render_template('login.html')
+    def error(self):
+        mensaje='Mensaje de prueba'
+        self.mi_error(mensaje)
+    def exce(self):
+        self.abort(500)
 
-class MainHandler(webapp2.RequestHandler):
-    def myMethod(self,user):
-        self.response.write("Hola " + user)
-    def ruta(self):
-    	self.response.write(self.request.application_url)
-    def agente(self):
-    	agente = self.request.headers.get('User-Agent')
-    	self.response.write(agente)
-    def device(self):
-        agente = self.request.headers.get('User-Agent')
-        device = httpagentparser.simple_detect(agente)
-        self.response.write(device)
-    def opsi(self):
-    	agente = self.request.headers.get('User-Agent')
-    	device = httpagentparser.detect(agente)
-    	ua_string = agente
-    	user_agent = parse(ua_string)
-    	self.response.write(user_agent.os.family)
-    def jsonE(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        obj = {
-        'success': 'some var', 
-        'payload': 'some var',
-          } 
-        self.response.write(json.encode(obj))
-    def holaJS(self,user):
-        self.response.headers['Content-Type'] = 'application/json'
-        obj = {
-        'Hola ': user,
-          } 
-        self.response.write(json.encode(obj))
+class ServiciosHandler(BaseHandler):
+    def get(self):
+        from webapp2 import Route
+        template_val ={
+        'metaDescripcion' : 'Consultorio general Gerardo Sepulveda',
+        'metaKeyWords' : 'consultorio,gerardo,sepulveda',
+        'metaCreador' : 'Erik Villegas'
+        }
+        self.render_template('index.html',**template_val)
--- routes.py
+++ routes.py
@@ -4,19 +4,10 @@ import webapp2
 from webapp2 import Router
 
 _routes = [
-	webapp2.Route(r'/hola/<user>', handler='handlers.siteHandler.MainHandler', name='hola', handler_method='myMethod'),
-	webapp2.Route(r'/ruta', handler='handlers.siteHandler.MainHandler', name='ruta', handler_method='ruta'),
-	webapp2.Route(r'/agente', handler='handlers.siteHandler.MainHandler', name='ruta', handler_method='agente'),
-	webapp2.Route(r'/device', handler='handlers.siteHandler.MainHandler', name='ruta', handler_method='device'),
-	webapp2.Route(r'/os', handler='handlers.siteHandler.MainHandler', name='ruta', handler_method='opsi'),
-    
     #SB-ADMIN-V2
-	webapp2.Route(r'/', handler='handlers.sbHandler.SbAdmin', name='home'),
-	webapp2.Route(r'/login', handler='handlers.sbHandler.SbAdmin', name='login', handler_method='login'),
-	webapp2.Route(r'/error', handler='handlers.sbHandler.SbAdmin', name='error', handler_method='error'),
-	webapp2.Route(r'/exce', handler='handlers.sbHandler.SbAdmin', name='exce', handler_method='exce'),
+	webapp2.Route(r'/', handler='handlers.siteHandler.MainHandler', name='home'),
+	webapp2.Route(r'/login', handler='handlers.siteHandler.MainHandler', name='login', handler_method='login'),
+	webapp2.Route(r'/error', handler='handlers.siteHandler.MainHandler', name='error', handler_method='error'),
+	webapp2.Route(r'/exce', handler='handlers.siteHandler.MainHandler', name='exce', handler_method='exce'),
 
-	#API Routes
-	webapp2.Route(r'/api', handler='handlers.siteHandler.MainHandler', handler_method='jsonE'),
-	webapp2.Route(r'/api/hola/<user>', handler='handlers.siteHandler.MainHandler', handler_method='holaJS'),
 ]
